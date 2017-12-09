f13ccc42a02fe75a669d7d42f0cac1c3b5b0d5ac
Hanjoung Lee <waterets@gmail.com>
2017-12-09 21:16:27 +0900

    Add comment for IEvent

--- game/src/core/i_event.h
+++ game/src/core/i_event.h
@@ -3,6 +3,8 @@
 
 #include "event_effect.h"
 
+// IEvent is an interface for raising game events, i.e. for notifying observers
+
 class IEvent {
  public:
    virtual void RaiseEvent(EventEffect::Type, Unit*, void*) = 0;
