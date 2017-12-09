5d4a293228854a4ed2a7d005b797d6e820eff704
Hanjoung Lee <waterets@gmail.com>
2017-12-09 20:35:49 +0900

    Introduce IEvent

---
+++ game/src/core/i_event.h
@@ -0,0 +1,11 @@
+#ifndef I_EVENT_H_
+#define I_EVENT_H_
+
+#include "event_effect.h"
+
+class IEvent {
+ public:
+   virtual void RaiseEvent(EventEffect::EventEffectType, void*) = 0;
+};
+
+#endif // I_EVENT_H_
