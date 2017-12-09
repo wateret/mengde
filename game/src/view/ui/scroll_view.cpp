2fa333dff6db08c8d918f24131fc11a74be3424b
Hanjoung Lee <waterets@gmail.com>
2017-12-09 21:37:45 +0900

    Remove LOG_DEBUG no more intrested

--- game/src/view/ui/scroll_view.cpp
+++ game/src/view/ui/scroll_view.cpp
@@ -26,21 +26,17 @@ bool ScrollView::OnMouseMotionEvent(const MouseMotionEvent e) {
 
 bool ScrollView::OnMouseWheelEvent(const MouseWheelEvent e) {
   if (e.IsLeft()) {
-    LOG_DEBUG("Left");
     coords_.x += kDefaultScrollAmount;
     coords_.x = std::min(coords_.x, 0);
   } else if (e.IsRight()) {
-    LOG_DEBUG("Right");
     coords_.x -= kDefaultScrollAmount;
     int x_min = std::min(0, GetFrame()->GetW() - view_->GetFrame()->GetW());
     coords_.x = std::max(coords_.x, x_min);
   }
   if (e.IsUp()) {
-    LOG_DEBUG("Up");
     coords_.y += kDefaultScrollAmount;
     coords_.y = std::min(coords_.y, 0);
   } else if (e.IsDown()) {
-    LOG_DEBUG("Down");
     coords_.y -= kDefaultScrollAmount;
     int y_min = std::min(0, GetFrame()->GetH() - view_->GetFrame()->GetH());
     coords_.y = std::max(coords_.y, y_min);
