a6f599bc794181f33a3d5a00d95f39c5b1d7d6c2
Hanjoung Lee <waterets@gmail.com>
2017-12-17 20:40:45 +0900

    Add missing color.cpp

---
+++ game/src/view/color.cpp
@@ -0,0 +1,42 @@
+#include "color.h"
+#include "logger.h"
+
+ColorGen::ColorGen() {
+  colors_.insert(std::make_pair("transparent", Color(255, 255, 255, 0)));
+  colors_.insert(std::make_pair("white", Color(255, 255, 255)));
+  colors_.insert(std::make_pair("black", Color(0, 0, 0)));
+  colors_.insert(std::make_pair("red", Color(255, 0, 0)));
+  colors_.insert(std::make_pair("green", Color(0, 255, 0)));
+  colors_.insert(std::make_pair("blue", Color(0, 0, 255)));
+  colors_.insert(std::make_pair("gray", Color(128, 128, 128)));
+  colors_.insert(std::make_pair("darkgray", Color(64, 64, 64)));
+  colors_.insert(std::make_pair("yellow", Color(255, 255, 0)));
+  colors_.insert(std::make_pair("lightyellow", Color(255, 255, 224)));
+  colors_.insert(std::make_pair("navy", Color(0, 0, 128)));
+  colors_.insert(std::make_pair("orange", Color(255, 165, 0)));
+
+  colors_.insert(std::make_pair("gauge_stats", Color(64, 192, 64)));
+  colors_.insert(std::make_pair("gauge_bg", Color(128, 128, 128)));
+  colors_.insert(std::make_pair("gauge_hp", Color(64, 192, 64)));
+  colors_.insert(std::make_pair("gauge_mp", Color(64, 192, 224)));
+  colors_.insert(std::make_pair("gauge_exp", Color(199, 146, 234)));
+  colors_.insert(std::make_pair("gauge_hp_damage", Color(192, 64, 64)));
+}
+
+ColorGen* ColorGen::GetInstance() {
+  static ColorGen color_gen;
+  return &color_gen;
+}
+
+Color ColorGen::Generate(const std::string& name, uint8_t alpha) {
+  auto elem = colors_.find(name);
+  if (elem == colors_.end()) {
+    LOG_ERROR("Color name '%s' is not registered", name.c_str());
+    return {0, 0, 0, alpha}; // return black
+  } else {
+    Color color = elem->second;
+    color.a = alpha;
+    return color;
+  }
+}
+
