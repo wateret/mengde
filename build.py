906529515a86fbdade395dd824bba389e4b61d6b
Hanjoung Lee <waterets@gmail.com>
2018-02-03 15:50:08 +0900

    Enhance build.py

--- build.py
+++ build.py
@@ -5,6 +5,8 @@ import sys
 import subprocess
 import os
 import platform
+import multiprocessing
+import time
 
 def wrap_gen(color):
     return lambda s: color + s + "\033[0m"
@@ -18,13 +20,16 @@ def cmd_to_string(cmd, args = []):
     return ' '.join([cmd] + args)
 
 def fail(msg):
-    print
-    print wrap_red(msg)
-    print
+    print("")
+    print(wrap_red(msg))
+    print("")
     exit(1)
 
 def print_cmd(cmd, args):
-    print wrap_blue(cmd_to_string(cmd, args))
+    print(wrap_blue(cmd_to_string(cmd, args)))
+
+def print_header(msg):
+    print(wrap_yellow(msg))
 
 def run_cmd(cmd, args = []):
     print_cmd(cmd, args)
@@ -46,19 +51,31 @@ def parse_args():
 def main():
     options = parse_args()
 
+    # Fetch system info
     system, node, release, version, machine, processor = platform.uname()
-    build_dir = "build/%s.%s.%s" % (system, machine, options.buildtype)
+    cpu_count = multiprocessing.cpu_count()
 
+    print_header("Start CMake configuration")
+    build_dir = "build/%s.%s.%s" % (system, machine, options.buildtype)
     check_run_cmd("mkdir", ["-p", build_dir])
     os.chdir(build_dir)
     check_run_cmd("cmake", ["-DCMAKE_BUILD_TYPE=" + options.buildtype,
-                            "../../"])
+                            ("../" * (build_dir.count("/") + 1))])
 
-    make_args = ["-j8"]
+    # From here, support `Makefile` project only 
+
     if options.clean:
-        make_args += ["clean"]
+        print_header("Clean build directory")
+        check_run_cmd("make", ["clean"])
 
+    jobs = "-j" + str(cpu_count * 2)
+    make_args = [jobs]
+
+    print_header("Start build")
+    start_time = time.time()
     check_run_cmd("make", make_args)
+    elapsed_time = time.time() - start_time
+    print("Build Time is %d:%02d.%2d" % (elapsed_time // 60, elapsed_time % 60, (elapsed_time * 100) % 100))
 
 if __name__ == "__main__":
     main()
