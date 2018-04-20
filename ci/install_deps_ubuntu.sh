e731416dad7fdc2f3a68189235a72e3598186485
Hanjoung Lee <waterets@gmail.com>
2018-04-21 00:31:09 +0900

    Remove `sudo` from install_deps

--- ci/install_deps_ubuntu.sh
+++ ci/install_deps_ubuntu.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-sudo apt-get -qq update
-sudo apt-get install -y cmake libsdl2-dev libsdl2-ttf-dev liblua5.2-dev libboost-all-dev
+apt-get -qq update
+apt-get install -y cmake libsdl2-dev libsdl2-ttf-dev liblua5.2-dev libboost-all-dev
 
 exit 0
