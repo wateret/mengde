c5a4d228e2767fc67d40c6da6746ab30de485d2e
Hanjoung Lee <waterets@gmail.com>
2018-05-12 20:10:03 +0900

    [format-check] Non-zero exit code when changed
    When there is diff exist, `clang-format.sh` returns non-zero.

--- ci/clang-format.sh
+++ ci/clang-format.sh
@@ -6,4 +6,18 @@ PROJECT_PATH=$(git rev-parse --show-toplevel)
 SOURCE_PATH=$PROJECT_PATH/game
 CLANG_FORMAT="clang-format"
 
+echo "[clang-format] Format check begins."
+
 find "$SOURCE_PATH" -type f \( -name "*.h" -o -name "*.cc" \) -print0 | xargs -0 "$CLANG_FORMAT" -i -style=file
+
+set +e
+
+git diff-index --quiet HEAD --
+STATUS=$?
+
+if [ "$STATUS" -eq "0" ]; then
+   echo "[clang-format] Format check succeeded."
+else
+   echo "[clang-format] Format check failed. Please run 'git diff' to check out the changes."
+fi
+exit $STATUS
