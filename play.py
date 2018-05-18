4d3de87ed88e97aa27ad66a9dd0eabf9f0c7c702
SaeHie Park <saehie.park@gmail.com>
2018-05-18 12:43:04 +0900

    Add play support for RPi resolution (#67)
    This will add solution change for Raspberry Pi
    before and aftering running the game in play.py script
    as an option in full screen mode

--- play.py
+++ play.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import time
 import subprocess
 import platform
 import argparse
@@ -10,6 +11,8 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("buildtype", default="Debug", help="build type",
                         choices=["Debug", "Release"], nargs="?")
+    parser.add_argument("-r", "--rpi", help="Set resolution for Raspberry Pi",
+                        action="store_true")
     return parser.parse_args()
 
 def main():
@@ -24,8 +27,18 @@ def main():
     install_folder = "mengde"
     os.chdir(install_folder)
 
+    if (options.rpi):
+        subprocess.call(['tvservice', '-e', 'DMT 16']) # change to 1024x768
+        time.sleep(1) # need a little bit of time for graphics to be stable
+
     game_path = os.path.join(".", "game")
     subprocess.call([game_path])
+    
+    if (options.rpi):
+        subprocess.call(["tvservice", "-p"]) # change to prefered(default) mode
+        time.sleep(1)
+        subprocess.call(["fbset", "-depth", "24"]) # hack to make screen visible
+        subprocess.call(["fbset", "-depth", "32"])
 
 if __name__ == "__main__":
     main()
