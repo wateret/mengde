ad09d9b33a43b54bbfcfa5d3273a195fddb97d71
Hanjoung Lee <waterets@gmail.com>
2018-05-22 18:05:25 +0900

    Add more tests for Vec2D

--- game/test/util/vec2d.cc
+++ game/test/util/vec2d.cc
@@ -9,3 +9,16 @@ BOOST_AUTO_TEST_CASE(Test1) {
   Vec2D exp{2, 4};
   BOOST_CHECK(vec + vec == exp);
 }
+
+BOOST_AUTO_TEST_CASE(Test2) {
+  Vec2D vec{1, 2};
+  Vec2D exp{2, 4};
+  BOOST_CHECK(vec * 2 == exp);
+}
+
+BOOST_AUTO_TEST_CASE(Test3) {
+  Vec2D vec1{1, 2};
+  Vec2D vec2{3, 4};
+  Vec2D exp{4, 6};
+  BOOST_CHECK(vec1 + vec2 == exp);
+}
