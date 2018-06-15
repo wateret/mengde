179ebe42bd96acb8f6f240b7a28081c05b53dbd2
llFreetimell <seok9311@naver.com>
2018-06-16 02:43:24 +0900

    Modify codes of checking Ubuntu version

--- ci/install_deps_ubuntu.sh
+++ ci/install_deps_ubuntu.sh
@@ -4,7 +4,10 @@ set -e
 
 sudo apt-get -qq update
 
-if [[ `lsb_release -rs` -lt 16 ]]
+version=`lsb_release -rs`
+version_int=${version:0:2}
+
+if [ $version_int -ge 16 ]
 then
 sudo apt-get install -y cmake libsdl2-dev libsdl2-ttf-dev liblua5.2-dev libboost-all-dev clang-format
 else
