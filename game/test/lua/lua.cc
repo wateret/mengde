dbb0c4d369f55f7ef064a04b7e0134ad175e8db4
Hanjoung Lee <waterets@gmail.com>
2018-08-10 01:48:38 +0900

    Update lua test

--- game/test/lua/lua.cc
+++ game/test/lua/lua.cc
@@ -4,7 +4,7 @@
 
 #include "lua/lua.h"
 
-BOOST_AUTO_TEST_CASE(FunctionCall1) {
+BOOST_AUTO_TEST_CASE(CallLuaFuncFromCpp_1) {
   ::lua::Lua l;
   l.RunScript(
       std::string("function foo()\n"
@@ -14,7 +14,7 @@ BOOST_AUTO_TEST_CASE(FunctionCall1) {
   BOOST_CHECK(ret == 1);
 }
 
-BOOST_AUTO_TEST_CASE(FunctionCall2) {
+BOOST_AUTO_TEST_CASE(CallLuaFuncFromCpp_2) {
   ::lua::Lua l;
   l.RunScript(
       std::string("function add(a, b)\n"
@@ -26,25 +26,32 @@ BOOST_AUTO_TEST_CASE(FunctionCall2) {
 
 extern "C" {
 
-static ::lua::Ref g_ref{-1};
+static ::lua::Ref g_ref_1{-1};
 
-static int add_callback(lua_State* L) {
+static int set_callback(lua_State* L) {
   ::lua::Lua l{L};
   ::lua::Ref ref = l.Pop<::lua::Ref>();
-  g_ref = ref;
+  g_ref_1 = ref;
   return 0;
 }
-}
+} // extern "C"
 
-BOOST_AUTO_TEST_CASE(FunctionCall_Ref1) {
+//
+// CalLuaCallbackFromCpp
+// 1. Register C function to Lua which registers lua::Ref to g_ref
+// 2. Call C function from Lua to add a Lua callback
+// 3. Call Lua function with g_ref(a lua::Ref) from C++
+
+BOOST_AUTO_TEST_CASE(CallLuaFunctionWithRefFromCpp_1) {
   ::lua::Lua l;
-  l.Register("add_callback", add_callback);
+  l.Register("set_callback", set_callback);
   l.RunScript(
       std::string("function add(a, b)\n"
                   "  return a + b\n"
                   "end\n"
-                  "add_callback(add)\n"
+                  "set_callback(add)\n"
                   ""));
-  int ret = l.Call<int>(g_ref, 10, 20);
+  int ret = l.Call<int>(g_ref_1, 10, 20);
   BOOST_CHECK(ret == 30);
 }
+
