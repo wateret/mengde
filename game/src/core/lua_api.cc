e0e6dc1d92518d3d06998c6d0b52b5e968c47248
Hanjoung Lee <waterets@gmail.com>
2018-09-03 00:31:51 +0900

    Minor update

--- game/src/core/lua_api.cc
+++ game/src/core/lua_api.cc
@@ -15,11 +15,12 @@ static Vec2D GetVec2DFromLua(lua::Lua* lua) {
 
 LUA_IMPL(AppointHero) {
   lua::Lua lua(L);
+
   uint16_t level = lua.Pop<uint16_t>();
   string id = lua.Pop<string>();
-  Stage* game = lua.Pop<Stage*>();
+  Stage* stage = lua.Pop<Stage*>();
 
-  game->AppointHero(id, level);
+  stage->AppointHero(id, level);
 
   return 0;
 }
@@ -28,9 +29,9 @@ LUA_IMPL(GenerateOwnUnit) {
   lua::Lua lua(L);
   Vec2D pos = GetVec2DFromLua(&lua);
   string id = lua.Pop<string>();
-  Stage* game = lua.Pop<Stage*>();
+  Stage* stage = lua.Pop<Stage*>();
 
-  int unit_no = game->GenerateOwnUnit(id, pos);
+  int unit_no = stage->GenerateOwnUnit(id, pos);
 
   lua.PushToStack(unit_no);
   return 1;
@@ -42,9 +43,9 @@ LUA_IMPL(GenerateUnit) {
   Force force = (Force)lua.Pop<int>();
   uint16_t level = lua.Pop<uint16_t>();
   string id = lua.Pop<string>();
-  Stage* game = lua.Pop<Stage*>();
+  Stage* stage = lua.Pop<Stage*>();
 
-  int unit_no = game->GenerateUnit(id, level, force, pos);
+  int unit_no = stage->GenerateUnit(id, level, force, pos);
 
   lua.PushToStack(unit_no);
   return 1;
@@ -54,18 +55,18 @@ LUA_IMPL(ObtainEquipment) {
   lua::Lua lua(L);
   uint16_t amount = lua.Pop<uint16_t>();
   string id = lua.Pop<string>();
-  Stage* game = lua.Pop<Stage*>();
+  Stage* stage = lua.Pop<Stage*>();
 
-  game->ObtainEquipment(id, amount);
+  stage->ObtainEquipment(id, amount);
 
   return 0;
 }
 
 LUA_IMPL(GetNumEnemiesAlive) {
   lua::Lua lua(L);
-  Stage* game = lua.Pop<Stage*>();
+  Stage* stage = lua.Pop<Stage*>();
 
-  uint32_t ret = game->GetNumEnemiesAlive();
+  uint32_t ret = stage->GetNumEnemiesAlive();
 
   lua_pushnumber(L, ret);
   return 1;
@@ -73,9 +74,9 @@ LUA_IMPL(GetNumEnemiesAlive) {
 
 LUA_IMPL(GetNumOwnsAlive) {
   lua::Lua lua(L);
-  Stage* game = lua.Pop<Stage*>();
+  Stage* stage = lua.Pop<Stage*>();
 
-  uint32_t ret = game->GetNumOwnsAlive();
+  uint32_t ret = stage->GetNumOwnsAlive();
 
   lua_pushnumber(L, ret);
   return 1;
@@ -85,9 +86,9 @@ LUA_IMPL(GetUnitInfo) {
   lua::Lua lua(L);
 
   auto uid = UId{lua.Pop<uint32_t>()};
-  Stage* game = lua.Pop<Stage*>();
+  Stage* stage = lua.Pop<Stage*>();
 
-  auto unit = game->GetUnit(uid);
+  auto unit = stage->GetUnit(uid);
 
   // Build a lua table as a return value
 
@@ -110,10 +111,10 @@ LUA_IMPL(GetUnitInfo) {
 LUA_IMPL(PushCmdMove) {
   lua::Lua lua(L);
   Vec2D pos = GetVec2DFromLua(&lua);
-  int unit_id = lua.Pop<int>();
-  Stage* game = lua.Pop<Stage*>();
+  UId uid{lua.Pop<uint32_t>()};
+  Stage* stage = lua.Pop<Stage*>();
 
-  game->Push(std::make_unique<CmdMove>(unit_id, pos));
+  stage->Push(std::make_unique<CmdMove>(uid, pos));
 
   return 0;
 }
@@ -121,10 +122,10 @@ LUA_IMPL(PushCmdMove) {
 LUA_IMPL(PushCmdSpeak) {
   lua::Lua lua(L);
   string words = lua.Pop<string>();
-  int unit_id = lua.Pop<int>();
-  Stage* game = lua.Pop<Stage*>();
+  UId uid{lua.Pop<uint32_t>()};
+  Stage* stage = lua.Pop<Stage*>();
 
-  game->Push(std::make_unique<CmdSpeak>(unit_id, words));
+  stage->Push(std::make_unique<CmdSpeak>(uid, words));
 
   return 0;
 }
@@ -133,10 +134,10 @@ LUA_IMPL(PushCmdGainExp) {
   lua::Lua lua(L);
 
   int exp = lua.Pop<int>();
-  int uid = lua.Pop<int>();
-  Stage* game = lua.Pop<Stage*>();
+  UId uid{lua.Pop<uint32_t>()};
+  Stage* stage = lua.Pop<Stage*>();
 
-  game->Push(std::make_unique<CmdGainExp>(uid, exp));
+  stage->Push(std::make_unique<CmdGainExp>(uid, exp));
 
   return 0;
 }
@@ -145,9 +146,9 @@ LUA_IMPL(SetOnDeploy) {
   lua::Lua lua(L);
 
   auto ref = lua.Pop<lua::Ref>();
-  Stage* game = lua.Pop<Stage*>();
+  Stage* stage = lua.Pop<Stage*>();
 
-  game->SetOnDeploy(ref);
+  stage->SetOnDeploy(ref);
 
   return 0;
 }
@@ -156,9 +157,9 @@ LUA_IMPL(SetOnBegin) {
   lua::Lua lua(L);
 
   auto ref = lua.Pop<lua::Ref>();
-  Stage* game = lua.Pop<Stage*>();
+  Stage* stage = lua.Pop<Stage*>();
 
-  game->SetOnBegin(ref);
+  stage->SetOnBegin(ref);
 
   return 0;
 }
@@ -167,9 +168,9 @@ LUA_IMPL(SetOnVictory) {
   lua::Lua lua(L);
 
   auto ref = lua.Pop<lua::Ref>();
-  Stage* game = lua.Pop<Stage*>();
+  Stage* stage = lua.Pop<Stage*>();
 
-  game->SetOnVictory(ref);
+  stage->SetOnVictory(ref);
 
   return 0;
 }
@@ -178,9 +179,9 @@ LUA_IMPL(SetOnDefeat) {
   lua::Lua lua(L);
 
   auto ref = lua.Pop<lua::Ref>();
-  Stage* game = lua.Pop<Stage*>();
+  Stage* stage = lua.Pop<Stage*>();
 
-  game->SetOnDefeat(ref);
+  stage->SetOnDefeat(ref);
 
   return 0;
 }
@@ -189,9 +190,9 @@ LUA_IMPL(SetEndCondition) {
   lua::Lua lua(L);
 
   auto ref = lua.Pop<lua::Ref>();
-  Stage* game = lua.Pop<Stage*>();
+  Stage* stage = lua.Pop<Stage*>();
 
-  game->SetEndCondition(ref);
+  stage->SetEndCondition(ref);
 
   return 0;
 }
@@ -201,9 +202,9 @@ LUA_IMPL(RegisterEvent) {
 
   auto handler = lua.Pop<lua::Ref>();
   auto condition = lua.Pop<lua::Ref>();
-  Stage* game = lua.Pop<Stage*>();
+  Stage* stage = lua.Pop<Stage*>();
 
-  auto id = game->RegisterEvent(condition, handler);
+  auto id = stage->RegisterEvent(condition, handler);
 
   lua.PushToStack(id);
 
@@ -214,9 +215,9 @@ LUA_IMPL(UnregisterEvent) {
   lua::Lua lua(L);
 
   auto id = lua.Pop<uint32_t>();
-  Stage* game = lua.Pop<Stage*>();
+  Stage* stage = lua.Pop<Stage*>();
 
-  game->UnregisterEvent(id);
+  stage->UnregisterEvent(id);
 
   return 0;
 }
