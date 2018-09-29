26aad60a1755f0053064b77b67d076846bb6b235
Hanjoung Lee <waterets@gmail.com>
2018-09-29 19:48:54 +0900

    [CI] Fix macOS failure

--- ci/install_deps_macos.sh
+++ ci/install_deps_macos.sh
@@ -3,12 +3,13 @@
 # Disabled below option due to python2 (2to3) link issue
 # set -e
 
-brew update
+brew update > /dev/null
 
 PACKAGES=(python2 cmake sdl2 sdl2_ttf lua boost clang-format)
 
 for dep in "${PACKAGES[@]}"; do
-  brew info "${dep}" | grep --quiet 'Not installed' && brew install "${dep}"
+  echo "Installing $dep"
+  brew info "${dep}" | grep --quiet 'Not installed' && brew install "${dep}" > /dev/null
 done
 
 exit 0
