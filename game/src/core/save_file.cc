223c5f8a7d6290b8cf2a4c7536757e8cbb3e11e3
Hanjoung Lee <waterets@gmail.com>
2019-03-10 20:36:24 +0900

    [core] Basic SaveFile de/serialization

--- game/src/core/save_file.cc
+++ game/src/core/save_file.cc
@@ -1,9 +1,39 @@
 #include "save_file.h"
 
+#include <fstream>
+
 namespace mengde {
 namespace core {
 
-SaveFile::SaveFile() {}
+SaveFile::SaveFile(const Path& path) : path_{path} {}
+
+void SaveFile::Serialize(const int* scenario) {
+  flatbuffers::FlatBufferBuilder builder(1024);
+  save::ScenarioBuilder sce_builder(builder);
+  sce_builder.add_test_value(*scenario);
+  auto sce = sce_builder.Finish();
+  builder.Finish(sce);
+
+  uint8_t* buffer = builder.GetBufferPointer();
+  uint32_t size = builder.GetSize();
+
+  std::ofstream file(path_.ToString(), std::ios::binary);
+  file.write(reinterpret_cast<const char*>(buffer), size);
+}
+
+int* SaveFile::Deserialize() {
+  std::ifstream file(path_.ToString(), std::ios::binary | std::ios::ate);
+  std::streamsize size = file.tellg();
+  file.seekg(0, std::ios::beg);
+
+  std::vector<char> buffer(size);
+  file.read(buffer.data(), size);
+
+  auto sce = save::GetScenario(buffer.data());
+  auto val = sce->test_value();
+
+  return new int{val};
+}
 
 }  // namespace core
 }  // namespace mengde
--- game/src/core/save_file.h
+++ game/src/core/save_file.h
@@ -1,16 +1,23 @@
 #ifndef MENGDE_CORE_SAVE_FILE_H_
 #define MENGDE_CORE_SAVE_FILE_H_
 
+#include <memory>
+
+#include "scenario.h"
 #include "scenario_generated.h"
+#include "util/path.h"
 
 namespace mengde {
 namespace core {
 
 class SaveFile {
  public:
-  SaveFile();
+  SaveFile(const Path& path);
+  void Serialize(const int* scenario);
+  int* Deserialize();
 
  private:
+  Path path_;
 };
 
 }  // namespace core
